1449aa21ffebcfd5b141dd0c0e86a68a90563165
beankeji-cloud <13515105030@163.com>
2025-09-26 14:45:20 +0800

    UPDATE least_resource.go

--- pkg/controller/manager/scheduler/priority/least_resource.go
+++ pkg/controller/manager/scheduler/priority/least_resource.go
@@ -1,3 +1,21 @@
+﻿// =======================================================================
+// Copyright 2021 The LiteIO Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// =======================================================================
+// Modifications by The SLiteIO Authors on 2025:
+// - Modification : support lvm thin volume
+
 package priority
 
 import (
@@ -22,6 +40,10 @@ func PriorityByLeastResource(ctx context.Context, n *state.Node, vol *v1.Antstor
 	if total <= 0 {
 		klog.Errorf("found StoragePool %s total space is invalid", n.Info.ID)
 		return 0
+	}
+	if n.Pool.IsThin {
+		total *= n.Pool.OverprovisionRatio
+		free = float64(n.Pool.GetVgVirtualFreeBytes())
 	}
 
 	score = int((total - free) / total * 100)
